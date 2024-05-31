72adb5b76853b9c54a8f588d744e57452ceda865
Luisito Lucas <128021291+lucasluisito1@users.noreply.github.com>
2024-05-31 14:51:52 +0800

    Update Assessment.sol

--- Metacrafters/contracts/Assessment.sol
+++ Metacrafters/contracts/Assessment.sol
@@ -1,60 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    event Deposit(address indexed account, uint256 amount);
+    event Withdraw(address indexed account, uint256 amount);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
 
-    constructor(uint initBalance) payable {
+    constructor(uint256 initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+        emit Deposit(msg.sender, _amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
+            revert InsufficientBalance(balance, _withdrawAmount);
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
+        emit Withdraw(msg.sender, _withdrawAmount);
+    }
 
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
+    function transfer(address _to, uint256 _amount) public {
+        require(_to != address(0), "Invalid recipient");
+        require(_to != owner, "You can't transfer to the owner");
+        require(balance >= _amount, "Insufficient balance");
+        
+        balance -= _amount;
+        emit Transfer(msg.sender, _to, _amount);
+    }
 
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+    function changeOwner(address payable _newOwner) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        owner = _newOwner;
     }
 }
