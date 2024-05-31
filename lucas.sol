c86420ecfd6b9bc241129195eff4a1616a5471cb
Luisito Lucas <128021291+lucasluisito1@users.noreply.github.com>
2024-05-31 23:13:56 +0800

    Update lucas.sol

--- lucas.sol
+++ lucas.sol
@@ -1,51 +1,52 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
 
-contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-    event Deposit(address indexed account, uint256 amount);
-    event Withdraw(address indexed account, uint256 amount);
-    event Transfer(address indexed from, address indexed to, uint256 amount);
+contract BasicToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balances;
 
-    constructor(uint256 initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _initialSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _initialSupply * 10**uint256(_decimals);
+        balances[msg.sender] = totalSupply;
     }
 
-    function getBalance() public view returns (uint256) {
-        return balance;
+    function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0), "Invalid recipient address");
+    require(_value > 0, "Value must be greater than zero");
+    require(_value <= balances[msg.sender], "Insufficient balance");
+    require(_value <= 100, "Transfer amount exceeds the limit"); 
+
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+}
+
+
+
+    function assertTest() public pure {
+        assert(1 == 2);
     }
 
-    function deposit(uint256 _amount) public payable {
-        require(msg.sender == owner, "You are not the owner of this account");
-        balance += _amount;
-        emit Deposit(msg.sender, _amount);
+    function revertTest() public pure {
+        revert("This is a revert test");
     }
 
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance(balance, _withdrawAmount);
-        }
-        balance -= _withdrawAmount;
-        emit Withdraw(msg.sender, _withdrawAmount);
-    }
-
-    function transfer(address _to, uint256 _amount) public {
-        require(_to != address(0), "Invalid recipient");
-        require(_to != owner, "You can't transfer to the owner");
-        require(balance >= _amount, "Insufficient balance");
-        
-        balance -= _amount;
-        emit Transfer(msg.sender, _to, _amount);
-    }
-
-    function changeOwner(address payable _newOwner) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        owner = _newOwner;
+    function requireTest(uint256 _value) public pure {
+        require(_value > 0, "Value must be greater than zero");
     }
 }
